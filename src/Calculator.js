420f10b0d49ada6991c8c7642b90558aad12cc20
Clayton Davidson <cldavidson@cn.edu>
2019-08-26 13:07:11 -0400

    Getting rid of excess files

---
+++ src/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Calculator.css';
+
+function Calculator() {
+  return (
+    <div className="Calculator">
+
+    </div>
+  );
+}
+
+export default Calculator;
